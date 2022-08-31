f67141a61f8b48ec62b243d4fefc2ef5828992ba
mstevenson <mbstevenson96@gmail.com>
2022-08-31 00:05:57 -0400

    complete steps 1-6 on lab and reset button add

---
+++ js/app.js
@@ -0,0 +1,33 @@
+
+//* -----------CONSTANTS----------//
+const btn = document.getElementById('submit-button')
+// console.dir(btn)
+
+const inputElement = document.getElementById('inputArea')
+// console.log(inputElement)
+
+const ulElement = document.getElementById('todo-list')
+// console.log(ulElement)
+
+const resetBtn = document.getElementById('reset-button')
+// console.log(resetBtn);
+
+
+
+//* -----------EVENT LISTENERS----------//
+btn.addEventListener('click', (event) => {
+  const li = document.createElement('li')
+  li.textContent = inputElement.value
+  // console.log(li)
+  if(inputElement.value === '') {}
+  else {
+    document.querySelector('ul').appendChild(li)
+  }
+  inputElement.value = ''
+})
+
+resetBtn.addEventListener('click', (event) => {
+  ulElement.innerHTML = ''
+});
+
+
